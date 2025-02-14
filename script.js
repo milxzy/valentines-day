d2841e9c04461fc42b65bc5e7668b02a259dde9f
Miles <milescollins9@gmail.com>
2025-02-14 11:42:13 -0600

    init

---
+++ script.js
@@ -0,0 +1,29 @@
+const noButton = document.getElementById('no-btn');
+
+noButton.addEventListener('mouseover', () => {
+
+  noButton.style.position = 'absolute';
+  moveButton(noButton);
+});
+
+function moveButton(button) {
+  const container = document.querySelector('.container');
+  const containerRect = container.getBoundingClientRect();
+  const buttonRect = button.getBoundingClientRect();
+
+  // Calculate new position within the container bounds
+  const maxX = containerRect.width - buttonRect.width;
+  const maxY = containerRect.height - buttonRect.height;
+
+  const newX = Math.random() * maxX;
+  const newY = Math.random() * maxY;
+
+  button.style.left = `${newX}px`;
+  button.style.top = `${newY}px`;
+}
+
+
+const yesButton = document.getElementById('yes-btn');
+yesButton.addEventListener('click', () => {
+  alert("Yay! You made me the happiest person! 💖");
+});
